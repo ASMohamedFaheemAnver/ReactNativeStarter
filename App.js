9ef9ced41b1da3701457089aad716395bca7fa0b
FreeDoM <jstrfaheem065@gmail.com>
2022-06-08 17:49:06 +0530

    Stack navigation configured,

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import LanguageProvider from '@provider/LanguageProvider';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import {persistor, store} from '@redux/store';
 import Child from '@screens/Child';
 import React from 'react';
@@ -6,11 +8,17 @@ import {Provider as ReduxProvider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 
 const App = () => {
+  const Stack = createStackNavigator();
+
   return (
     <ReduxProvider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <LanguageProvider loading={null}>
-          <Child></Child>
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen name="Child" component={Child} />
+            </Stack.Navigator>
+          </NavigationContainer>
         </LanguageProvider>
       </PersistGate>
     </ReduxProvider>
