9653bdb95d54fab4e6e4aa807c4fd2e125b983e0
FreeDoM <jstrfaheem065@gmail.com>
2022-06-09 22:20:26 +0530

    Vector icons added,

--- app/navigations/BottomTabStack.js
+++ app/navigations/BottomTabStack.js
@@ -3,13 +3,29 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from '@screens/Home';
 import Profile from '@screens/Profile';
 import React from 'react';
+import EntypoIcon from 'react-native-vector-icons/Entypo';
+import IoniconsIcon from 'react-native-vector-icons/Ionicons';
 
 const BottomTabStack = () => {
   const BottomTab = createBottomTabNavigator();
   return (
     <BottomTab.Navigator initialRouteName={RouteNames.Home}>
-      <BottomTab.Screen name={RouteNames.Home} component={Home} />
-      <BottomTab.Screen name={RouteNames.Profile} component={Profile} />
+      <BottomTab.Screen
+        options={{
+          tabBarIcon: ({color, focused, size}) => <EntypoIcon name="home" />,
+        }}
+        name={RouteNames.Home}
+        component={Home}
+      />
+      <BottomTab.Screen
+        options={{
+          tabBarIcon: ({color, focused, size}) => (
+            <IoniconsIcon name="person" />
+          ),
+        }}
+        name={RouteNames.Profile}
+        component={Profile}
+      />
     </BottomTab.Navigator>
   );
 };
